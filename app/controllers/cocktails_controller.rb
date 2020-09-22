b611dba085ac749c79a91e3f0bf6b4cedc7df6ac
Eduardo Dantas <eduardopimenteldantas@hotmail.com>
2020-09-21 21:19:45 -0300

    teste 1

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -18,7 +18,6 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    @cocktail.save
     if @cocktail.save
       redirect_to cocktail_path(@cocktail.id)
     else
@@ -29,7 +28,7 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 
 
